69c3e999e00b82a7cb2ca082add84252a5a5aad2
Owen Melia <owenjmelia@gmail.com>
2018-12-21 12:34:24 -0500

    Working on evaluation

--- ADABoost.py
+++ ADABoost.py
@@ -1,5 +1,5 @@
 import numpy as np
-def adaboost(weak_learner, m, T, data, labels):
+def adaboost(weak_learner, m, T, data, labels, call_1, call_2, args_1 = None, args_2 = None):
     '''
     An implementation of the ADABoost algorithm as taught by Prof. Nati Srebro.
 
@@ -13,6 +13,10 @@ def adaboost(weak_learner, m, T, data, labels):
         data: The source of data on which the boosting algorithm learns
             \\WHAT IS THE DATATYPE??
         labels: A list of labels corresponding to the data
+        call_1: function call for training the weak learner
+        call_2: function call for using the weak learner for prediction
+        args_1: (optional) additional arguments for call_1
+        args_2: (optional) additional arguments for call_2
     '''
 
     #Initialization
@@ -26,30 +30,68 @@ def adaboost(weak_learner, m, T, data, labels):
     for i in range(T):
         continue
         #Create new sample object accroding to distribution
-        (sample_data, sample_labels) = sample_creator(data, labels, distribution, m)
+        sample_indices = sample_creator(distribution, m)
+        data_sample = data[sample_indices]
+        label_sample = labels[indices]
 
-        new_learner = weak_learner(sample_data, sample_labels)
-        (epsilon, delta) = evaluate(weak_learner)
+        new_learner = weak_learner(data_sample, label_sample)
+        (epsilon, alpha, distribution) = evaluate(new_learner,
+                                                    distribution,
+                                                    data,
+                                                    labels,
+                                                    call_2,
+                                                    args_2)
 
     return
 
-def sample_creator(data, labels, distribution, m):
+def sample_creator(distribution, m):
     '''
     Creates a new sample of size <m> by randomly sampling from <distribution>,
     a discrete probability distribution over the cases in <data x labels>
     Inputs:
-        data: the whole dataset
-        labels: the whole labelset
         distribution: a list of floats of length equal to that of data and labels
         m: size of the sample to be created
     Returns:
-        sample_data: a list of length m of data
-        sample_labels: a list of length m of labels
+        sample_indices: a list of indices which indicate the new sample
     '''
-    indices = np.arange(len(data))
+    indices = np.arange(len(distribution))
     sample_indices = np.random.choice(indices, m, True, distribution)
-    data_sample = data[sample_indices]
-    label_sample = labels[indices]
+    return sample_indices
 
 
-    return (data_sample, label_sample)
+def evaluate(new_learner, distribution, data, labels, call, args = None):
+    '''
+    Evaluates the performance of the new weak learner on the training data
+    Inputs:
+        weak_learner: a machine learning model produced in one iteration of
+            adaboost
+        distribution: the current sample distribution
+        call: function call to evaluate the weak learning algorithm
+    '''
+    source_size = len(data)
+    errors = np.zeros(source_size, dtype=int)
+    if args == None:
+        for i in range(source_size):
+            iteration_error = call(new_learner, data[i], labels[i])
+            if iteration_error:
+                errors[i] = 1
+    else:
+        for i in range(source_size):
+            iteration_error = call(new_learner, data[i], labels[i], args)
+            if iteration_error:
+                errors[i] = 1
+    #Epsilon is the loss of the new predictor on the old distribution
+    epsilon = np.sum(np.dot(errors, distribution))
+
+    #Alpha is a measure of our confidence in the new predictor
+    alpha = .5 * np.log(1 / epsilon - 1)
+
+    #Create the nultiplicative component of the new distribution:
+    #Cases with errors are up-weighted and cases without errors are down-weighted
+    new_dist = np.exp(alpha * (-1)**(1 + errors))
+
+    #Apply this to the old distribution
+    new_dist = np.dot(new_dist, distribution)
+    new_dist = distribution / np.sum(new_dist)
+
+    return (epsilon, alpha, new_dist)
--- testing_adaboost.py
+++ testing_adaboost.py
@@ -3,11 +3,19 @@ import numpy as np
 
 
 np.random.seed(100)
-sample_array_1 = np.reshape(np.random.randint(0, 10, 20), (5,2,2))
-sample_array_2 = np.random.randint(0, 10, 5)
-sample_distribution = np.array([.2] * 5)
+sample_data_1 = np.reshape(np.random.randint(0, 10, 20), (5,2,2))
+sample_labels_1 = np.random.randint(0, 10, 5)
+sample_distribution_1 = np.array([.2] * 5)
 
 def test_sample_creator_1():
-    sample = ADABoost.sample_creator(sample_array_1, sample_array_2,
-                sample_distribution, 7)
-    assert np.shape(sample[0]) == (7,2,2)
+    sample = ADABoost.sample_creator(sample_distribution_1, 7)
+    assert np.shape(sample) == (7,)
+
+def test_sample_creator_2():
+    sample = ADABoost.sample_creator(sample_distribution_1, 3)
+    assert np.shape(sample) == (3,)
+
+sample_distribution_2 = np.array([1,0,0,0,0,0,0])
+def test_sample_creator_3():
+    sample = ADABoost.sample_creator(sample_distribution_2, 4)
+    np.testing.assert_array_equal(sample, [0,0,0,0])
